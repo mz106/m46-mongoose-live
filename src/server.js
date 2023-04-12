c7f4bc4becfa221df8e1aa7e39b3a92950b79aca
Michael Z <michaelzwierzanski@gmail.com>
2023-04-12 15:44:06 +0100

    add post route to add book to db

--- src/server.js
+++ src/server.js
@@ -3,8 +3,29 @@ require("./db/connection");
 
 const express = require("express");
 
+const Book = require("./books/model");
+
 const app = express();
 
 app.use(express.json());
+
+app.post("/books/addbook", async (req, res) => {
+  try {
+    const newBook = await Book.create({
+      author: req.body.author,
+      genre: req.body.genre,
+      title: req.body.title,
+    });
+
+    const successResponse = {
+      message: "success",
+      newBook: newBook,
+    };
+
+    res.status(201).json(successResponse);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.listen(5001, () => console.log("Server is listening"));
